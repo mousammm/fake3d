a857693ec90fef3a2d1e76aabb97790e3f97d361
mousammm <mousamssdia@gmail.com>
2026-09-01 23:40:48 +0530

    update events

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "constants.h"
 #include "sdl_gfx.c"
 
@@ -7,28 +8,12 @@ int main()
   sdl_init("software renderer", SCREEN_WIDTH, SCREEN_HEIGHT);
 
   SDL_Event e;
-  int running = 1;
+  bool running = true;
   const Uint8* KEYS = SDL_GetKeyboardState(NULL);
     
   while(running) {
 
-    while(SDL_PollEvent(&e)) {
-      if (e.type == SDL_QUIT) { running = 0; }
-      else if (e.type == SDL_KEYDOWN) {
-        switch (e.key.keysym.sym) {
-          case SDLK_w:
-            printf("w pressed\n");
-            break;
-        }
-      } else if (e.type == SDL_KEYUP) {
-        switch (e.key.keysym.sym) {
-          case SDLK_w:
-            printf("w released\n");
-            break;
-        }
-      }
-    }
-
+    while(SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) { running = false; } }
     if (KEYS[SDL_SCANCODE_ESCAPE]) { running = 0; }
     if (KEYS[SDL_SCANCODE_W]) { printf("w pressed\n"); }
     if (KEYS[SDL_SCANCODE_S]) { printf("s pressed\n"); }
