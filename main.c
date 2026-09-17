116bae5cf298b474469f767b67626dc8b473270d
mousammm <mousamssdia@gmail.com>
2026-09-18 03:36:15 +0530

    add loadOBJ stuff and add mesh dynamic triangle and count

--- main.c
+++ main.c
@@ -28,10 +28,70 @@ uint32_t getColor(float dp) {
   return argbColor;
 }
 
+mesh_t loadOBJFile(const char* filename) {
+  mesh_t mesh = { .tri = NULL, .triangle_count = 0 };
+  FILE* file = fopen(filename, "r");
+  if (!file) {
+    printf("Error: Could not open file %s\n", filename);
+    return mesh;
+  }
+
+  char line[128];
+  int vertex_count = 0;
+  int face_count = 0;
+
+  // Pass 1: Count vertices and faces for memory allocation
+  while (fgets(line, sizeof(line), file)) {
+    if (line[0] == 'v' && line[1] == ' ') {
+      vertex_count++;
+    } else if (line[0] == 'f' && line[1] == ' ') {
+      face_count++;
+    }
+  }
+
+  // Allocate array to hold temporary vertices
+  vec3d_t* verts = (vec3d_t*)malloc(vertex_count * sizeof(vec3d_t));
+  // Allocate the actual triangles into your mesh struct
+  mesh.tri = (triangle_t*)malloc(face_count * sizeof(triangle_t));
+  mesh.triangle_count = face_count;
+
+  // Rewind file to start parsing actual data
+  rewind(file);
+
+  int v_idx = 0;
+  int f_idx = 0;
+
+  while (fgets(line, sizeof(line), file)) {
+    // Parse individual Vertex
+    if (line[0] == 'v' && line[1] == ' ') {
+      sscanf(line, "v %lf %lf %lf", &verts[v_idx].x, &verts[v_idx].y, &verts[v_idx].z);
+      v_idx++;
+    }
+    // Parse individual Face (Triangle)
+    else if (line[0] == 'f' && line[1] == ' ') {
+      int f1, f2, f3;
+      // Note: OBJ indices are 1-based, C arrays are 0-based (hence the -1)
+      sscanf(line, "f %d %d %d", &f1, &f2, &f3);
+      
+      mesh.tri[f_idx].p[0] = verts[f1 - 1];
+      mesh.tri[f_idx].p[1] = verts[f2 - 1];
+      mesh.tri[f_idx].p[2] = verts[f3 - 1];
+      mesh.tri[f_idx].color = 0xFFFFFFFF; // Default to fully opaque white ARGB
+      
+      f_idx++;
+    }
+  }
+
+  fclose(file);
+  free(verts); // Free temporary vertex array
+  return mesh;
+}
+
 int main(int argc, char** argv) {
   sh_gfx* gfx = sh_gfx_init("fake3d", SWIDTH, SHEIGHT);
 
-  mesh_t cube      = create_cube();
+  // mesh_t cube      = create_cube();
+  mesh_t cube      = loadOBJFile("./ship.obj");
   mat4x4_t matProj = create_perspective_matrix(SWIDTH, SHEIGHT, FOV, NEAR_PLANE, FAR_PLANE);
   float fTheta     = 0.0f; // Angle tracker for rotation
 
@@ -56,7 +116,7 @@ int main(int argc, char** argv) {
     fTheta         += 60.0f * delta_time;
     mat4x4_t matRot = create_rotation_matrix(0.0f, fTheta, fTheta);
 
-    for (int i = 0; i < 12; ++i) {
+    for (int i = 0; i < cube.triangle_count; ++i) {
       triangle_t tri = cube.tri[i];
       triangle_t triProjected, triTranslated, triRotated;
 
@@ -117,13 +177,8 @@ int main(int argc, char** argv) {
         }
 
         // DRAW THE CUBE
-        uint32_t wfColor = 0xFFFFFFFF;
-        // sh_gfx_draw_line(gfx, triProjected.p[0].x, triProjected.p[0].y, triProjected.p[1].x, triProjected.p[1].y, wfColor);
-        // sh_gfx_draw_line(gfx, triProjected.p[1].x, triProjected.p[1].y, triProjected.p[2].x, triProjected.p[2].y, wfColor);
-        // sh_gfx_draw_line(gfx, triProjected.p[2].x, triProjected.p[2].y, triProjected.p[0].x, triProjected.p[0].y, wfColor);
-
         sh_gfx_fill_triangle(gfx, triProjected.p[0].x, triProjected.p[0].y, triProjected.p[1].x, triProjected.p[1].y, triProjected.p[2].x, triProjected.p[2].y, triProjected.color);
-        //sh_gfx_draw_triangle(gfx, triProjected.p[0].x, triProjected.p[0].y, triProjected.p[1].x, triProjected.p[1].y, triProjected.p[2].x, triProjected.p[2].y, 0xFFFF0000);
+        sh_gfx_draw_triangle(gfx, triProjected.p[0].x, triProjected.p[0].y, triProjected.p[1].x, triProjected.p[1].y, triProjected.p[2].x, triProjected.p[2].y, 0xFF000000);
       }
     }
 
